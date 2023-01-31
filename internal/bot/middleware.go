56c096699baedf871ee6abb686b79f9aebd395b9
gaasb <xyzgaa@gmail.com>
2023-01-31 05:43:28 +0300

    + middleware for user restriction commands, updated libs

--- internal/bot/middleware.go
+++ internal/bot/middleware.go
@@ -1,6 +1,20 @@
 package bot
 
+import (
+	"errors"
+	"golang.org/x/exp/slices"
+	"gopkg.in/telebot.v3"
+)
+
+type Administrator struct {
+	UserID int64      `bson:"_id" json:"_id"`
+	Rights []UserType `bson:"rights" json:"rights"`
+}
+type AdministratorRoleAndRights struct {
+	Rights []UserType `bson:"rights" json:"rights"`
+}
 type UserType uint
+type UserRole string
 
 const (
 	PostEdit UserType = iota
@@ -8,10 +22,14 @@ const (
 	CreateTag
 )
 
+var administrators = map[int64][]UserType{}
+
 const (
 	RoleAdmin     = "admin"
 	RoleModerator = "mod"
 )
+
+var adminOnlyController *telebot.Group
 
 var userRights = map[UserType]string{
 	PostEdit:   "Edit message",
@@ -52,4 +70,30 @@ func UpdateRosterList() {
 func AddRoleToUser(user interface{}, role string) {
 	_ = Role{}
 	//rosterList = append(rosterList, user, r.RoleByName(role))
+}
+
+func ForAdministrators(right UserType) telebot.MiddlewareFunc {
+	return func(next telebot.HandlerFunc) telebot.HandlerFunc {
+		return func(ctx telebot.Context) error {
+			if admin, ok := administrators[ctx.Sender().ID]; ok && isContainRight(admin, right) {
+				return next(ctx)
+			}
+			return nil
+		}
+
+	}
+}
+func AddAdmin(userID int64, rights ...UserType) error {
+	if err := InsertAdministrator(&Administrator{UserID: userID, Rights: rights}); err != nil {
+		return errors.New("Already in db")
+	}
+	administrators[userID] = rights
+	return nil
+}
+func InitAllAdministrators() {
+	administrators = FindAllAdministrators()
+
+}
+func isContainRight(role []UserType, right UserType) bool {
+	return slices.Contains(role, right)
 }
